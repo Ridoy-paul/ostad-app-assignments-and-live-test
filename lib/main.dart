3c226235b2939baabd6eace5155036b94a6ece43
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-16 23:36:49 +0600

    module 6 live test added.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Shopping BD",
       debugShowCheckedModeBanner: false,
       home: HomePageActivity(),
     );
@@ -15,6 +16,14 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePageActivity extends StatelessWidget {
+  final List<Map<String, dynamic>> shoppingItems = [
+    {"name": "Apples", "icon": Icons.shopping_cart},
+    {"name": "Bananas", "icon": Icons.shopping_cart},
+    {"name": "Milk", "icon": Icons.shopping_cart},
+    {"name": "Bread", "icon": Icons.shopping_cart},
+    {"name": "Eggs", "icon": Icons.shopping_cart},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,34 +36,18 @@ class HomePageActivity extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
         ],
       ),
-      body: HomePageActivity(),
+      body: ListView.builder(
+        itemCount: shoppingItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(shoppingItems[index]["icon"]),
+            title: Text(shoppingItems[index]["name"]),
+            onTap: () {
+
+            },
+          );
+        },
+      ),
     );
   }
-}
-
-class HomePageBody extends StatelessWidget {
-  final List<Map<String, dynamic>> shoppingItems = [
-    {"name": "Apples", "icon": Icons.shopping_cart},
-    {"name": "Bananas", "icon": Icons.shopping_cart},
-    {"name": "Milk", "icon": Icons.shopping_cart},
-    {"name": "Bread", "icon": Icons.shopping_cart},
-    {"name": "Eggs", "icon": Icons.shopping_cart},
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: shoppingItems.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(shoppingItems[index]["icon"]),
-          title: Text(shoppingItems[index]["name"]),
-          onTap: () {
-
-          },
-        );
-      },
-    );
-  }
-
 }
