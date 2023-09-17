35489ea7f03f5d6063b116f8e8be04cecb2165ef
Ridoy-paul <cse.ridoypaul@gmail.com>
2023-09-18 01:22:20 +0600

    module 6 assignment updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,17 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePageActivity extends StatelessWidget {
-  const HomePageActivity({super.key});
+  HomePageActivity({super.key});
+
+  final List<String> imageUrls = [
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+    'https://topmobileinfo.com/images/products/samsung-w23-mobile-8782.png',
+
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -46,11 +56,44 @@ class HomePageActivity extends StatelessWidget {
                   focusedBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.black87),
                   ),
-
-                  ///labelText: 'Search for Photos...',
-
                 ),
-              )
+              ),
+              SizedBox(height: 10.0,),
+              IconButton(onPressed: () {
+                MySnackbar.showSnackBar(context, "This is Home");
+              }, icon: Icon(Icons.call)),
+
+              GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemCount: 6,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                    onTap: () {
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //   SnackBar(
+                      //     content: Text('Image $index clicked!'),
+                      //   ),
+                      // );
+                    },
+                    child: Card(
+                      child: Column(
+                        children: [
+                          Image.network(
+                            imageUrls[index], // Use the image URL from the list
+                            height: 120.0,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Caption for Image $index'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ],
           ),
 
@@ -58,12 +101,24 @@ class HomePageActivity extends StatelessWidget {
       ),
     );
   }
+}
 
+class MySnackbar {
+  static void showSnackBar(BuildContext context, String mesage) {
+    final snackBar = SnackBar(
+      content: Text(mesage),
+      duration: Duration(seconds: 1),
+      action: SnackBarAction(
+        label: 'Dismiss',
+        onPressed: () {},
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 }
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget{
   const MyAppBar({super.key});
-
   @override
   Widget build(BuildContext context) {
     return AppBar(
